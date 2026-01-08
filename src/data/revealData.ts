f1d34a3dc6f1c9ba4755b0d6b510b6984c40c31b
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2026-01-08 16:37:58 +0100

    potentially all texts in place

--- src/data/revealData.ts
+++ src/data/revealData.ts
@@ -9,8 +9,8 @@ export const revealData: RevealEntry[] = [
   ["Your efficient dealing with life matters", "Words any fool could understand E"],
   ["How small, seemingly insignificant, but often cute things can steal your attention", "I wonder if there could be a better way F"],
   ["The incredible warmth with which you've accepted me and those who come with me", "Than this simple truth: G"],
-  ["", "H"],
-  ["", "*ahem* I "],
-  ["", "Telling others about you J"],
-  ["", "Invariably sounds like bragging K"],
+  ["How you are just as adult as you need to be but not more", "H"],
+  ["That you would always give me the love I deserve", "*ahem* I "],
+  ["Your interest in things that interest me, and my culture", "Telling others about you J"],
+  ["Your spiritual depth", "Invariably sounds like bragging K"],
 ];
