21c2e6e80e64b108b7b4b85bae4691467cdae283
Christofer Ohlsson <christofer.ohlsson@gmail.com>
2026-01-08 16:38:39 +0100

    removing trailing ordering letters

--- src/data/revealData.ts
+++ src/data/revealData.ts
@@ -2,15 +2,15 @@
 export type RevealEntry = [string, string];
 
 export const revealData: RevealEntry[] = [
-  ["Your mental processing speed", "If I were to distill A"],
-  ["Your integrity and absolute need for individuality", "The nature of what makes you B"],
-  ["Your talent for being goofy and silly", "Impossible not to fall for C"],
-  ["Your musical gifts", "Down into simple terms D"],
-  ["Your efficient dealing with life matters", "Words any fool could understand E"],
-  ["How small, seemingly insignificant, but often cute things can steal your attention", "I wonder if there could be a better way F"],
-  ["The incredible warmth with which you've accepted me and those who come with me", "Than this simple truth: G"],
-  ["How you are just as adult as you need to be but not more", "H"],
-  ["That you would always give me the love I deserve", "*ahem* I "],
-  ["Your interest in things that interest me, and my culture", "Telling others about you J"],
-  ["Your spiritual depth", "Invariably sounds like bragging K"],
+  ["Your mental processing speed", "If I were to distill"],
+  ["Your integrity and absolute need for individuality", "The nature of what makes you"],
+  ["Your talent for being goofy and silly", "Impossible not to fall for"],
+  ["Your musical gifts", "Down into simple terms"],
+  ["Your efficient dealing with life matters", "Words any fool could understand"],
+  ["How small, seemingly insignificant, but often cute things can steal your attention", "I wonder if there could be a better way"],
+  ["The incredible warmth with which you've accepted me and those who come with me", "Than this simple truth"],
+  ["How you are just as adult as you need to be but not more", ""],
+  ["That you would always give me the love I deserve", "*ahem*"],
+  ["Your interest in things that interest me, and my culture", "Telling others about you"],
+  ["Your spiritual depth", "Invariably sounds like bragging"],
 ];
